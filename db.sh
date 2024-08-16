b4722b9161469d958021209124f326481c695a3f
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-15 23:41:30 -0700

    ALL

--- db.sh
+++ db.sh
@@ -48,7 +48,7 @@ VALIDATE $? "start mysqld"
 mysql -h db.narendra.shop -uroot -p${mysql_root_password} -e 'show databases' &>>$LOG_FILE
 if [ $? -eq 0 ]
 then 
-    echo "Root password already set $Y SKIPPING $N"
+    echo -e "Root password already set $Y SKIPPING $N"
 else 
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG_FILE
     VALIDATE $? "mysql root password"    
